315b08a3ff785382863e7fff6448998e23f0fa3d
OlgaHollister <olya778899@gmail.com>
2017-04-18 13:13:23 -0400

    spring-boot app

---
+++ src/main/java/mainpackage/UploadingController.java
@@ -0,0 +1,33 @@
+package mainpackage;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@Controller
+public class UploadingController {
+    public static final String uploadInDir = System.getProperty("user.dir") + "/uploadingdir/";
+
+    @RequestMapping("/")
+    public String uploading(Model model) {
+        File file = new File(uploadInDir);
+        model.addAttribute("files", file.listFiles());
+        return "uploading";
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String uploadingPost(@RequestParam("uploadingFiles") MultipartFile[] uploadingFiles) throws IOException {
+        for(MultipartFile uploadedFile : uploadingFiles) {
+            File file = new File(uploadInDir + uploadedFile.getOriginalFilename());
+            uploadedFile.transferTo(file);
+        }
+
+        return "redirect:/";
+    }
+}
